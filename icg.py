e67ad3da259ebb52d1e6706aef6105d105cd3dc2
Hope Kagame <kagame458@protonmail.com>
2022-05-04 06:41:35 +0300

    Add while loop generation

--- icg.py
+++ icg.py
@@ -219,10 +219,127 @@ def gen_if_clause(tup):
             temp += 1
             snippet += '\n(NOT, T{}, T{})'.format(temp, (temp-1))
             temp += 1
-        snippet += '\n(IF, T{}, GOTO LABEL L{})'.format((temp-1), label)
+        snippet += '\n(IF, T{}, GOTO L{})'.format((temp-1), label)
         label += 1
         snippet += '\n*'
-        snippet += '\n(LABEL L{})'.format((label-1))
+        snippet += '\n(LABEL, L{})'.format((label-1))
+
+    snippet = snippet.replace('\n*', gen_else_clause(tup[2]))
+
+    return snippet
+
+
+def gen_while_clause(tup):
+    snippet = ''
+    global temp
+    global label
+
+    condition = tup[1]
+
+    snippet += '(LABEL L{})\n'.format((label))
+    label += 1
+
+    if condition[0] == 'boolean':
+        if condition[1] == '>':
+            snippet += '(GT, T{}, {}, {})'.format(temp,
+                                                  condition[2][1], condition[3][1])
+            temp += 1
+        elif condition[1] == '>=':
+            snippet += '(GE, T{}, {}, {})'.format(temp,
+                                                  condition[2][1], condition[3][1])
+            temp += 1
+        elif condition[1] == '<':
+            snippet += '(LT, T{}, {}, {})'.format(temp,
+                                                  condition[2][1], condition[3][1])
+            temp += 1
+        elif condition[1] == '<=':
+            snippet += '(LE, T{}, {}, {})'.format(temp,
+                                                  condition[2][1], condition[3][1])
+            temp += 1
+        elif condition[1] == '==':
+            snippet += '(EQ, T{}, {}, {})'.format(temp,
+                                                  condition[2][1], condition[3][1])
+            temp += 1
+        else:
+            snippet += '(NEQ, T{}, {}, {})'.format(temp,
+                                                   condition[2][1], condition[3][1])
+            temp += 1
+
+        snippet += '\n(NOT, T{}, T{})'.format((temp), (temp-1))
+        temp += 1
+
+    else:
+        # For logical
+        # First clause
+        if condition[2][1] == '>':
+            snippet += '(GT, T{}, {}, {})'.format(temp,
+                                                  condition[2][2][1], condition[2][3][1])
+            temp += 1
+        elif condition[2][1] == '>=':
+            snippet += '(GE, T{}, {}, {})'.format(temp,
+                                                  condition[2][2][1], condition[2][3][1])
+            temp += 1
+        elif condition[2][1] == '<':
+            snippet += '(LT, T{}, {}, {})'.format(temp,
+                                                  condition[2][2][1], condition[2][3][1])
+            temp += 1
+        elif condition[2][1] == '<=':
+            snippet += '(LE, T{}, {}, {})'.format(temp,
+                                                  condition[2][2][1], condition[2][3][1])
+            temp += 1
+        elif condition[2][1] == '==':
+            snippet += '(EQ, T{}, {}, {})'.format(temp,
+                                                  condition[2][2][1], condition[2][3][1])
+            temp += 1
+        else:
+            snippet += '(NEQ, T{}, {}, {})'.format(temp,
+                                                   condition[2][2][1], condition[2][3][1])
+            temp += 1
+
+        # Second clause
+        if condition[3][1] == '>':
+            snippet += '\n(GT, T{}, {}, {})'.format(temp,
+                                                    condition[3][2][1], condition[3][3][1])
+            temp += 1
+        elif condition[3][1] == '>=':
+            snippet += '\n(GE, T{}, {}, {})'.format(temp,
+                                                    condition[3][2][1], condition[3][3][1])
+            temp += 1
+        elif condition[3][1] == '<':
+            snippet += '\n(LT, T{}, {}, {})'.format(temp,
+                                                    condition[3][2][1], condition[3][3][1])
+            temp += 1
+        elif condition[3][1] == '<=':
+            snippet += '\n(LE, T{}, {}, {})'.format(temp,
+                                                    condition[3][2][1], condition[3][3][1])
+            temp += 1
+        elif condition[3][1] == '==':
+            snippet += '\n(EQ, T{}, {}, {})'.format(temp,
+                                                    condition[3][2][1], condition[3][3][1])
+            temp += 1
+        else:
+            snippet += '\n(NEQ, T{}, {}, {})'.format(temp,
+                                                     condition[3][2][1], condition[3][3][1])
+            temp += 1
+
+        # Combine Logical
+        if condition[1] == '&&':
+            snippet += '\n(AND, T{}, T{}, T{})'.format(temp,
+                                                       (temp-2), (temp-1))
+            temp += 1
+            snippet += '\n(NOT, T{}, T{})'.format(temp, (temp-1))
+            temp += 1
+        else:
+            snippet += '\n(OR, T{}, T{}, T{})'.format(temp, (temp-2), (temp-1))
+            temp += 1
+            snippet += '\n(NOT, T{}, T{})'.format(temp, (temp-1))
+            temp += 1
+
+    snippet += '\n(IF, T{}, GOTO L{})'.format((temp-1), label)
+    label += 1
+    snippet += '\n*'
+    snippet += '\n(GOTO, L{})'.format((label-2))
+    snippet += '\n(LABEL, L{})'.format((label-1))
 
     snippet = snippet.replace('\n*', gen_else_clause(tup[2]))
 
@@ -257,6 +374,9 @@ def gen_else_clause(tup):
 
             if tup[1][1][0] == 'else clause':
                 snippet += '\n{}'.format(gen_else_clause(tup[1][1]))
+
+            if tup[1][1][0] == 'while loop':
+                snippet += '\n{}'.format(gen_while_clause(tup[1][1]))
 
             tup = tup[2]
 
@@ -300,6 +420,9 @@ def generate_code(body):
 
             if body[1][1][0] == 'else clause':
                 code += '{}'.format(gen_else_clause(body[1][1][1]))
+
+            if body[1][1][0] == 'while loop':
+                code += '{}'.format(gen_while_clause(body[1][1]))
 
             body = body[2]
 
